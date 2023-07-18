9d8d1751cfeac1531669572086f84c3d03aec007
stopyc <e13242597082@outlook.com>
2023-07-18 20:42:02 +0800

    fix:修改接受消息的监听类的bug

--- mq-service/src/main/java/org/example/listener/AcceptMessageListener.java
+++ mq-service/src/main/java/org/example/listener/AcceptMessageListener.java
@@ -2,14 +2,14 @@ package org.example.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.event.AcceptMessageEvent;
-import org.example.websocket.MyWebSocket;
+import org.example.pojo.vo.WsMessageVO;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
  * @program: util
- * @description: 用户上线监听器
+ * @description: 用户接受消息监听器
  * @author: stop.yc
  * @create: 2023-07-18 09:46
  **/
@@ -20,7 +20,6 @@ public class AcceptMessageListener {
     @Async
     @EventListener(classes = AcceptMessageEvent.class)
     public void handleEvent(AcceptMessageEvent acceptMessageEvent) {
-        MyWebSocket myWebSocket = acceptMessageEvent.getMyWebSocket();
-        //GlobalWsMap.online(myWebSocket);
+        WsMessageVO wsMessageVO = acceptMessageEvent.getWsMessageVO();
     }
 }
