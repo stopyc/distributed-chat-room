ff3c322fb07468e217e3d604b42a914c3fa1251c
stopyc <e13242597082@outlook.com>
2023-11-06 13:03:14 +0800

    fix:修改配置文件

--- gateway/src/main/java/org/example/util/GateWayHashUtils.java
+++ gateway/src/main/java/org/example/util/GateWayHashUtils.java
@@ -1,7 +1,6 @@
 package org.example.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.example.util.RedisUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
